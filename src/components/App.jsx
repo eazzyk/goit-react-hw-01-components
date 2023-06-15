75d2ac2ef4a637078649d4056d170b20d12cf1ee
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-06-15 06:02:28 +0300

    іуц

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { Profile } from './Profile/Profile';
+import user from 'data/user';
+
 export const App = () => {
-  return <div>React homework sssssssstemplate</div>;
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
